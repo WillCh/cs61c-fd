0cd5d8d81acca5062320170cf7584672f580cf23
Class Account <cs61c-fd@freyburg.cs.berkeley.edu>
2014-06-24 18:28:59 -0700

    final changes.

--- lab01/hello.c
+++ lab01/hello.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
-int main(int argc, char *argv[]) {
-  printf("Hello World!\n");
-  return 0;
-=======
 int main(int argc, char **argv) {
   printf("Hello World!\n");
-  return;
->>>>>>> lab01make
+  return 0;
+
 }
